c7fa27ac0d8ee54eff58cd9755c09e766eb0aac2
Jack-a-Lope <jack@kellens.com>
2026-01-26 15:35:06 -0500

    Added parseInput to map index to preference list. TODO: use the Entity class

--- helper.py
+++ helper.py
@@ -19,3 +19,27 @@ class Entity:
     # def unmatch(self, other):
     #     self.isMatched = False
     #     self.matchedTo = None
+
+def parseInput(filepath, hospitals, hQueue, students):
+    with open(filepath, 'r') as input:
+        lines = input.readlines()
+
+    rows = []
+    for line in lines:
+        rows.append(line.strip())
+    
+    n = int(rows[0])
+
+    for i in range(1, n + 1):
+        preferences = []
+        for c in range(n):
+            preferences.append(int(rows[i][c]))
+        hospitals[i] = preferences[:]
+        hQueue.append(i)
+    for i in range(n + 1, 2*n + 1):
+        preferences = []
+        for c in range(n):
+            preferences.append(int(rows[i][c]))
+        students[i-n] = preferences[:]
+    return n
+        
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from helper import parseInput
+
 def main():
-    print("Working please but it's Nick turn")
+    hospitals = {}
+    hQueue = []
+    students = {}
+    n = 0
+
+    n = parseInput("Data\Input\\testInput.txt", hospitals, hQueue, students)
+    print("Hospitals: ", hospitals, "\nStudents: ", students, "\nQueue", hQueue, "\nn:", n)
+    
 
 if __name__ == "__main__":
     main()
